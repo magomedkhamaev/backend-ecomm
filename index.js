6d8f23690f36b5ec75e9c4b7dfab09c913bbf58e
magomedkhamaev <125343031+magomedkhamaev@users.noreply.github.com>
2023-02-24 14:46:12 +0300

    fix cors

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
   {
-  origin: ['http://localhost:3000'],
+  origin: ['https://keen-centaur-b5df4f.netlify.app'],
   credentials: true,
 }
 ));
