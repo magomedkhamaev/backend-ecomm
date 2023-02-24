c0c3d0b5fb3db8155ce85ae429b71dd8e73a50cc
magomedkhamaev <125343031+magomedkhamaev@users.noreply.github.com>
2023-02-24 12:23:17 +0300

    first commit

---
+++ models/cartModel.js
@@ -0,0 +1,30 @@
+import  mongoose from "mongoose"; // Erase if already required
+
+// Declare the Schema of the Mongo model
+const  CartSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        count: Number,
+        color: String,
+        price: Number,
+      },
+    ],
+    cartTotal: Number,
+    totalAfterDiscount: Number,
+    orderby: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Export the model
+export default mongoose.model("Cart", CartSchema);
